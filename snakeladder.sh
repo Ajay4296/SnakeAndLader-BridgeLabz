582a8c400950c2bfc82c6ca6c498202a96654180
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-15 14:42:22 +0530

    [Ajay]Add.implementation of usecase_3

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,3 +1,79 @@
-#!/bin/bash
-declare -A  GameBoard[10,10]
-echo "welcome to snake and ladder game"
+#!/bin/bash/
+Ladder()
+{
+case $1 in 
+2)
+ladder=23
+return $ladder
+;;
+
+7)
+ladder=29
+return $ladder
+;;
+
+22)
+ladder=41
+return $ladder
+;;
+
+28)
+ladder=77
+return $ladder
+;;
+
+30)
+ladder=44
+return $ladder
+;;
+
+70)
+ladder=90
+return $ladder
+;;
+
+87)
+ladder=93
+return $ladder
+;;
+
+*)
+ladder=0
+return $ladder
+;;
+}
+
+Snake()
+{
+case in $?
+27)
+snake=7
+return $snake
+;;
+
+39)
+snake=3
+return $snake
+;;
+
+50)
+snake=34
+return $snake
+;;
+
+66)
+snake=24
+return $snake
+;;
+
+73)
+snake=12
+return $snake
+;;
+
+99)
+snake=26
+return
+72
+;;
+}

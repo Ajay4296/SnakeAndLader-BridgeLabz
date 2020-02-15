ff6ccdaa8150f1f28fbd9da4b0bdcf6af07cd8a3
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-15 13:16:47 +0530

    [Ajay]Add.implementation of UC-2 generate random number

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-declare -A  GameBoard[10,10]
-echo "welcome to snake and ladder game"
+#!/bin/bash/
+
+rollDice(){
+while(( playerPossition<100 ))
+do
+diceNumber=$(( $(( $RANDOM%6 ))+1 ))
+playerPossition=$(( $playerPossition+$diceNumber ))
+done
+
+
+
+
+
+
+

80361043a5e07896e7e18f525c1cdad049a74600
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-15 15:40:34 +0530

    [Ajay]Add.implementation of UC-4

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
-declare -A  GameBoard[10,10]
-echo "welcome to snake and ladder game"
+gamestart()
+{
+count=0
+playerPossition
+possition=$?
+while[ $possition -le 99 ]
+do
+rollDice
+number=$?
+count=$(($count+1))
+possition=$(($possition+$number))
+echo "at $count roll playerpossition is $possition"
+Ladder $possition
+ladder1=$?
+if((ladder1>=1))
+then
+possition=$(($possition+$ladder1))
+echo "at $count roll playerpossition is $possition"
+fi
+Snake $possition
+snake1=$?
+if((snake1>=1))
+then
+possition=$(($possition-$snake1))
+echo "at $count roll playerpossition is $possition"
+fi
+while(( $possition>100 ))
+do
+echo "at $count roll playerpossition is $possition"
+done
+done
+echo "player possition at $count th  roll is 100"
+
+
+
+

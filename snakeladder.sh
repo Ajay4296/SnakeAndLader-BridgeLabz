04cae701df8b37c288957069ed1c19463f646bdd
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-17 09:20:17 +0530

    [Ajay]Add.implentation of devlop branch

--- snakeladder.sh
+++ snakeladder.sh
@@ -42,3 +42,11 @@ return $currentposition
 }
 
 
+rollDice(){
+while(( playerPossition<100 ))
+do
+diceNumber=$(( $(( $RANDOM%6 ))+1 ))
+playerPossition=$(( $playerPossition+$diceNumber ))
+done
+
+

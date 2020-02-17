daa86c79bdaa353aeffcf977c992f160ff1376ce
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-17 09:15:33 +0530

    [Ajay]Add.implentation of devlop

--- snakeladder.sh
+++ snakeladder.sh
@@ -1,22 +1,44 @@
-declare -A  GameBoard[10,10]
 initializeGameBoard()
 {
 size=100
 for(( i=1;i<=10;i++ ))
-do 
+do
 if((i%2!=0))
 then
-for((j=1;j<=10;j++))
+for(( j=1;j<=10;j++ ))
 do
-GameBoard[i,j]=$size
+GameBoard[$i,$j]=$size
 size=$(($size-1))
 done
 else
-for((j=1;j<=10;j++))
+for(( j=10;j>=1;j-- ))
 do
-GameBoard[i,j]=$size
+GameBoard[$i,$j]=$size
 size=$(($size-1))
 done
 fi
 done
+}
+
+printGameBoard()
+{
+echo ========================================
+for((i=1;i<=10;i++))
+do
+for((j=1;j<=10;j++))
+do
+printf "| ${GameBoard[$i,$j]}"
+done
+echo "|"
+echo ========================================
+
+done
+}
+
+function InitializePlayerPosition()
+{
+currentpositition=0
+return $currentposition
+}
+
 
